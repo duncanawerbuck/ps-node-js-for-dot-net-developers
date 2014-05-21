0aebdabee3443273233dc8ff45fbdab501eb2f3d
githubUser919 <duk444@gmail.com>
2014-05-21 15:33:24 +0100

    Handle GET requests to api/users by returning JSON

--- TheBoard/TheBoard/server.js
+++ TheBoard/TheBoard/server.js
@@ -4,8 +4,25 @@ var express = require("express");
 var app = express();
 
 // Handle GET requests to the root
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
     res.send("<html><head></head><body><h1>Express FTW!</h1></body><html>");
+    console.log(req.url);
+});
+
+// Handle GET requests to api/users by returning JSON.
+app.get('/api/users', function (req, res) {
+
+    // object to return as JSON
+    var someJsObject = {
+        name: "Shawn",
+        isValid: true,
+        group: "Admin"
+    };
+
+    // specify content type explicitly in response header
+    //res.set("Content-Type", "application/json");
+
+    res.send(someJsObject);
     console.log(req.url);
 });
 
