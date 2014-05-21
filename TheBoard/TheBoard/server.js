5b2aec15829debd397d60c6bf4a01154452b7ff1
githubUser919 <duk444@gmail.com>
2014-05-21 14:38:10 +0100

    +very simple web server using http, not express yet.

--- TheBoard/TheBoard/server.js
+++ TheBoard/TheBoard/server.js
@@ -1,6 +1,15 @@
-﻿var http = require('http');
-var port = process.env.port || 1337;
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(port);
+﻿var http = require("http");
+
+var server = http.createServer(function(req, res) {
+
+    if (req.url === '/favicon.ico') { res.end();
+        return;
+    }
+
+    var htmlResponse = "<html><head></head><body><h1>" + req.url + "</h1></body>";
+    res.write(htmlResponse);
+    console.log('The req.url is: ' + req.url);
+    res.end();
+});
+
+server.listen(3000);
