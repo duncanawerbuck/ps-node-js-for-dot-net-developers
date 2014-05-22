ae2ad52a45f5143b79dbcabb14cbb2fe4eb588dc
githubUser919 <duk444@gmail.com>
2014-05-22 11:47:48 +0100

    Tidy up (remove EJS & Jade stuff).

--- TheBoard/TheBoard/server.js
+++ TheBoard/TheBoard/server.js
@@ -3,31 +3,14 @@ var express = require('express');
 
 var app = express();
 
-var ejsEngine = require('ejs-locals');
-
-// --- view engine: jade ---
-//app.set('view engine', 'jade');
-
-// --- view engine: ejs ---
-//app.engine('ejs', ejsEngine);   // support master pages
-//app.set('view engine', 'ejs'); // ejs view engine
-
-// --- view engine: vash ---
 app.set('view engine', 'vash');
 
 // Handle GET requests to the root
 app.get('/', function (req, res) {
-    
-    // --- view engine: jade ---
-    //res.render('jade/index', { title: 'Express + Jade' });
 
-    // --- view engine: ejs ---
-    // res.render('ejs/index', { title: 'Express + EJS' });
-
-    // --- view engine: vash ---
     res.render('index', { title: 'Express + Vash' });
-
     console.log(req.url);
+
 });
 
 // Handle GET requests to api/users by returning JSON.
