3c09bde9af30a5ff66aebb2482f7adcd651b80f5
githubUser919 <duk444@gmail.com>
2014-05-23 15:54:10 +0100

    Beginnings of js code to access MongoDB (up to 03:40).

---
+++ TheBoard/TheBoard/data/database.js
@@ -0,0 +1,10 @@
+﻿(function(database) {
+
+    var mongodb = require('mongodb');
+    var mongoUrl = 'mongodb://localhost:/27017/theBoard';
+    var theDb = null;
+
+    database.getDb = function(next) {
+
+    };
+})(module.exports);
