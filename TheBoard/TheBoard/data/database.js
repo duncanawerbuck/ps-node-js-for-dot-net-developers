d70c4eeef413419f1fb876b8bfa65333f21b9b5f
githubUser919 <duk444@gmail.com>
2014-05-26 15:31:55 +0100

    getDb function implemented in database.js

--- TheBoard/TheBoard/data/database.js
+++ TheBoard/TheBoard/data/database.js
@@ -2,9 +2,26 @@
 
     var mongodb = require('mongodb');
     var mongoUrl = 'mongodb://localhost:/27017/theBoard';
-    var theDb = null;
+    var theDb = null; // this will be re-used so the db is only created once (on first request).
 
-    database.getDb = function(next) {
-
+    database.getDb = function(next) { // the 'next' param is the callback function. Takes an error as first param, or the created db as the second.
+        if (!theDb) {
+            // connect to the db
+            mongodb.MongoClient.connect(mongoUrl, function(err, db) {
+                if (err) {
+                    next(err, null);
+                } else {
+                    // Shawn recommends that we wrap the db in an object so we can extend it easily later.
+                    theDb = {
+                        db: db
+                    };
+                    next(null, db);
+                }
+            });
+        } else { // db already exists...
+            next(null, // no error
+                theDb);
+        }
     };
+
 })(module.exports);
