379371b7f10773d36abd5edb5415f4f3a4a5731b
githubUser919 <duk444@gmail.com>
2014-05-29 13:53:13 +0100

    Return note collection (not wrapped in category).

--- TheBoard/TheBoard/controllers/notesController.js
+++ TheBoard/TheBoard/controllers/notesController.js
@@ -5,6 +5,8 @@
 
     notesController.init = function (app) {
 
+
+        // GET notes by category name
         app.get('/api/notes/:categoryName', function (req, res) {
 
             var categoryName = req.params.categoryName;
@@ -17,10 +19,27 @@
 
                     res.set('Content-Type', 'application/json'); // JSON is the default, but we're good citizens, so specifying content type explicitly.
 
-                    res.send(notes);
+                    res.send(notes.notes);
                 }
             });
         });
+
+        // POST - add a new note
+        app.post('/api/notes/:categoryName', function(req, res) {
+
+            var categoryName = req.params.categoryName;
+
+            var noteToInsert = {
+                note: req.body.note,
+                color: req.body.color,
+                author: 'Shawn Wildermuth'
+            };
+
+            data.addNote(categoryName, noteToInsert, function(err) {
+                
+            });
+        });
+
     };
 
 })(module.exports);
