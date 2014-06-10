7ad27ea0cfdd25e969bfd3ce302e4d25ee4baf26
githubUser919 <duk444@gmail.com>
2014-06-10 16:25:10 +0100

    Implemented new note form post - actually save.

--- TheBoard/TheBoard/public/js/notesView.js
+++ TheBoard/TheBoard/public/js/notesView.js
@@ -5,13 +5,9 @@
     theModule.controller('notesViewController', ['$window', '$http',
         function($window, $http) {
             var vm = this;
-
             vm.notes = [];
 
-            vm.newNote = {
-                note: '',
-                color: 'yellow'
-            };
+            vm.newNote = createBlankNote();
 
             // categoryName is last part of url...
             var urlParts = $window.location.pathname.split('/');
@@ -25,6 +21,26 @@
                 function(err) {
                     alert(err);
                 });
+
+            vm.save = function () {
+                $http.post(notesUrl, vm.newNote)
+                    .then(function(result) {
+                        // success
+                        vm.notes.push(result.data);
+                        vm.newNote = createBlankNote();
+                    },
+                        function(err) {
+                            alert(err);
+                        });
+            };
+
+            function createBlankNote() {
+                vm.newNote = {
+                    note: '',
+                    color: 'yellow'
+                };
+            }
+
         }
     ]);
 
