9463d2a03f52c3cfa3f12d9835a4bde75a74f2cd
githubUser919 <duk444@gmail.com>
2014-06-12 11:41:56 +0100

    Hashing and salting implemented. Don't tell the police.

--- TheBoard/TheBoard/auth/hasher.js
+++ TheBoard/TheBoard/auth/hasher.js
@@ -1,11 +1,23 @@
 ﻿(function(hasher) {
 
-    hasher.createSalt = function(parameters) {
-        //TODO: Implement me (will need a reference to crypto, which is available in node core).
+    var crypto = require('crypto');
+
+    hasher.createSalt = function() {
+        // create 8-character salt to assist in computing a more secure hash (see computeHash function). whales have big willies.
+        var len = 8;
+        return crypto.randomBytes(Math.ceil(len / 2)).toString('hex').substring(0, len);
     };
 
     hasher.computeHash = function(source, salt) {
-        //TODO: Implement me
+        
+        // need a hashing algorithm to create the hash
+        var hmac = crypto.createHmac('sha1', salt);
+
+        // create the hash itself
+        var hash = hmac.update(source);
+
+        // return the hash as a hexadecimal number
+        return hash.digest('hex');
     };
 
 })(module.exports);
--- TheBoard/TheBoard/auth/index.js
+++ TheBoard/TheBoard/auth/index.js
@@ -1,6 +1,7 @@
 ﻿(function(auth) {
 
     var data = require('../data');
+    var hasher = require('./hasher');
 
     auth.init = function(app) {
 
@@ -10,12 +11,15 @@
         );
 
         app.post('/register', function (req, res) {
+
+            var salt = hasher.createSalt();
+
             var user = {
                 name: req.body.name,
                 email: req.body.email,
                 username: req.body.username,
-                passwordHash: '',
-                salt: ''
+                passwordHash: hasher.computeHash(req.body.password, salt),
+                salt: salt
             };
 
             data.addUser(user, function(err) {
