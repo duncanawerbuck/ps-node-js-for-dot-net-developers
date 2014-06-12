63dd6d983b9c41d06d0c6e1592c4769b5a2595a2
githubUser919 <duk444@gmail.com>
2014-06-12 11:07:24 +0100

    +hasher to hash & salt passwords. Not complete.

---
+++ TheBoard/TheBoard/auth/hasher.js
@@ -0,0 +1,11 @@
+﻿(function(hasher) {
+
+    hasher.createSalt = function(parameters) {
+        //TODO: Implement me (will need a reference to crypto, which is available in node core).
+    };
+
+    hasher.computeHash = function(source, salt) {
+        //TODO: Implement me
+    };
+
+})(module.exports);
